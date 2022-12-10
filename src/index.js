180fd99de2880d74f80099f241cc919b336367dc
arcygore <rcgore602@gmail.com>
2022-12-10 15:33:44 -0500

    Update CSS for more readable page Adjusted code and added photos to commit per Dec 10 update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-import { contentLoad , clearContent } from './pageLoad.js';
+import { contentLoad ,  menuLoad , aboutLoad } from './pageLoad.js';
 
 const contentDiv = document.querySelector("#content");
 
 contentLoad();
 
-const clearButton = document.querySelector(".clear");
+const menuPage = document.querySelector(".menu");
 
-clearButton.addEventListener('click', clearContent);
+menuPage.addEventListener('click', menuLoad);
+
+const homePage = document.querySelector(".home");
+
+homePage.addEventListener('click', contentLoad);
+
+const aboutPage = document.querySelector(".about");
+
+aboutPage.addEventListener('click', aboutLoad);
--- src/pageLoad.js
+++ src/pageLoad.js
@@ -4,6 +4,8 @@ export function contentLoad() {
 
     const contentDiv = document.querySelector("#content");
 
+    contentDiv.innerHTML = '';
+
     const mainTitle = document.createElement('h1');
     mainTitle.innerText = "Welcome to Arcygore's Taco Shop!";
 
@@ -12,6 +14,46 @@ export function contentLoad() {
 
     const mainText = document.createElement('p');
     mainText.innerText = "Why yes, we do have mushroom tacos available!";
+
+    contentDiv.appendChild(mainTitle);
+    contentDiv.appendChild(mainImg);
+    contentDiv.appendChild(mainText);
+};
+
+export function aboutLoad() {
+
+    const contentDiv = document.querySelector("#content");
+
+    contentDiv.innerHTML = '';
+
+    const mainTitle = document.createElement('h1');
+    mainTitle.innerText = "About Us";
+
+    const mainImg = document.createElement('img');
+    mainImg.src = "../images/peppers.jpg";
+
+    const mainText = document.createElement('p');
+    mainText.innerText = "We pride ourselves on quality and use the best ingredients available to us";
+
+    contentDiv.appendChild(mainTitle);
+    contentDiv.appendChild(mainImg);
+    contentDiv.appendChild(mainText);
+};
+
+export function menuLoad() {
+
+    const contentDiv = document.querySelector("#content");
+
+    contentDiv.innerHTML = '';
+
+    const mainTitle = document.createElement('h1');
+    mainTitle.innerText = "Here is Arcygore's Menu!";
+
+    const mainImg = document.createElement('img');
+    mainImg.src = "../images/tofutaco.jpg";
+
+    const mainText = document.createElement('p');
+    mainText.innerText = "The tofu taco is to die for!";
 
     contentDiv.appendChild(mainTitle);
     contentDiv.appendChild(mainImg);
