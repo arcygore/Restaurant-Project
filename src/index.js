1f35b2aca248498bbf287ffc3260df856f86d491
arcygore <rcgore602@gmail.com>
2022-12-14 16:41:45 -0500

    Nav bar working, added JSON reader for menu create

--- src/index.js
+++ src/index.js
@@ -1,17 +1,3 @@
 import { contentLoad ,  menuLoad , aboutLoad } from './pageLoad.js';
 
-const contentDiv = document.querySelector("#content");
-
 contentLoad();
-
-const menuPage = document.querySelector(".menu");
-
-menuPage.addEventListener('click', menuLoad);
-
-const homePage = document.querySelector(".home");
-
-homePage.addEventListener('click', contentLoad);
-
-const aboutPage = document.querySelector(".about");
-
-aboutPage.addEventListener('click', aboutLoad);
--- src/pageLoad.js
+++ src/pageLoad.js
@@ -1,4 +1,4 @@
-function navBarLoad(content) { // This section of code is not working. I am still working the fix.
+function navBarLoad(content) { // Loads the Navigation Bar at the top of the page.
 
     const contentDiv = document.querySelector("#content");
 
@@ -10,12 +10,15 @@ function navBarLoad(content) { // This section of code is not working. I am stil
 
     const menuTab = document.createElement('li');
     menuTab.classList.add('menu', 'link');
+    menuTab.innerText = "Menu";
 
     const homeTab = document.createElement('li');
     homeTab.classList.add('home', 'link');
+    homeTab.innerText = "Home";
 
     const aboutTab = document.createElement('li');
     aboutTab.classList.add('about', 'link');
+    aboutTab.innerText = "About";
 
     navBarList.appendChild(menuTab);
     navBarList.appendChild(homeTab);
@@ -23,7 +26,21 @@ function navBarLoad(content) { // This section of code is not working. I am stil
     navBar.appendChild(navBarList);
     content.appendChild(navBar);
 
-    return content;
+}
+
+function pageEventListener() {
+    const menuPage = document.querySelector(".menu");
+
+    menuPage.addEventListener('click', menuLoad);
+
+    const homePage = document.querySelector(".home");
+
+    homePage.addEventListener('click', contentLoad);
+
+    const aboutPage = document.querySelector(".about");
+
+    aboutPage.addEventListener('click', aboutLoad);
+
 }
 
 export function contentLoad() {
@@ -32,7 +49,32 @@ export function contentLoad() {
 
     contentDiv.innerHTML = '';
 
-    navBarLoad;
+    // const navBar = document.createElement('header');
+    // navBar.classList.add("nav-bar");
+    // contentDiv.appendChild(navBar);
+
+    // const navBarList = document.createElement('ul')
+    // navBarList.classList.add('nav-links');
+
+    // const menuTab = document.createElement('li');
+    // menuTab.classList.add('menu', 'link');
+    // menuTab.innerText = "Menu";
+
+    // const homeTab = document.createElement('li');
+    // homeTab.classList.add('home', 'link');
+    // homeTab.innerText = "Home";
+
+    // const aboutTab = document.createElement('li');
+    // aboutTab.classList.add('about', 'link');
+    // aboutTab.innerText = "About";
+
+    // navBarList.appendChild(menuTab);
+    // navBarList.appendChild(homeTab);
+    // navBarList.appendChild(aboutTab);
+    // navBar.appendChild(navBarList);
+    // contentDiv.appendChild(navBar);
+
+    navBarLoad(contentDiv);
 
     const mainTitle = document.createElement('h1');
     mainTitle.innerText = "Welcome to Arcygore's Taco Shop!";
@@ -47,7 +89,48 @@ export function contentLoad() {
     contentDiv.appendChild(mainImg);
     contentDiv.appendChild(mainText);
 
+    pageEventListener();
 };
+
+let menuList = [{
+    "itemimgsrc": "../images/streettaco.jpg",
+    "itemname": "Street Taco",
+    "itemprice": 10.99,
+    "itemdescription": "Traditional Mexican street taco with your choice of chocolate, vanilla, or strawberry!"
+},{
+    "itemimgsrc": "../images/streettaco.jpg",
+    "itemname": "Street Taco",
+    "itemprice": 10.99,
+    "itemdescription": "Traditional Mexican street taco with your choice of chocolate, vanilla, or strawberry!"
+}
+]
+
+function createMenu(menuList, menuLocation) {
+    let i=1;
+    menuList.forEach(function(item) {
+        console.log(item);
+        const menuItem = document.createElement("div");
+        menuItem.classList.add('menu-item', `menu-item${i}`);
+    
+        const itemImg = document.createElement('img');
+        itemImg.classList.add('food-img');
+        itemImg.src = item.itemimgsrc;
+
+        const foodItemAndPrice = document.createElement('h2');
+        foodItemAndPrice.innerText = `${item.itemname} - $${item.itemprice}`;
+        console.log(foodItemAndPrice);
+
+        const mainText = document.createElement('p');
+        mainText.innerText = `${item.itemdescription}`;
+
+        menuItem.appendChild(itemImg);
+        menuItem.appendChild(foodItemAndPrice);
+        menuItem.appendChild(mainText);
+        
+        menuLocation.appendChild(menuItem);
+        i++;
+    });
+}
 
 export function aboutLoad() {
 
@@ -55,7 +138,7 @@ export function aboutLoad() {
 
     contentDiv.innerHTML = '';
 
-    navBarLoad();
+    navBarLoad(contentDiv);
 
     const mainTitle = document.createElement('h1');
     mainTitle.innerText = "About Us";
@@ -69,6 +152,7 @@ export function aboutLoad() {
     contentDiv.appendChild(mainTitle);
     contentDiv.appendChild(mainImg);
     contentDiv.appendChild(mainText);
+    pageEventListener();
 };
 
 export function menuLoad() {
@@ -77,20 +161,27 @@ export function menuLoad() {
 
     contentDiv.innerHTML = '';
 
-    navBarLoad();
+    navBarLoad(contentDiv);
 
     const mainTitle = document.createElement('h1');
     mainTitle.innerText = "Here is Arcygore's Menu!";
 
-    const mainImg = document.createElement('img');
-    mainImg.src = "../images/tofutaco.jpg";
 
-    const mainText = document.createElement('p');
-    mainText.innerText = "The tofu taco is to die for!";
+    const menuDiv = document.createElement("div");
+    menuDiv.classList.add('menu-container');
 
-    contentDiv.appendChild(mainTitle);
-    contentDiv.appendChild(mainImg);
-    contentDiv.appendChild(mainText);
+
+    // const menuItem = document.createElement("div");
+    // menuItem.classList.add('menu-item');
+
+    // const mainImg = document.createElement('img');
+    // mainImg.src = "../images/tofutaco.jpg";
+
+    // const mainText = document.createElement('p');
+    // mainText.innerText = "The tofu taco is to die for!";
+    createMenu(menuList, menuDiv);
+    contentDiv.appendChild(menuDiv);
+    pageEventListener();
 };
 
 export function clearContent() {
@@ -98,5 +189,6 @@ export function clearContent() {
     const contentDiv = document.querySelector("#content");
 
     contentDiv.innerHTML = '';
+    pageEventListener();
 
 }
